67a567c2d581662f69148964a0c308c3b1943885
Drew Anello <drew.anello@gmail.com>
2022-12-10 14:45:32 -0500

    adds pages page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Pages from './pages/Pages'
 function App () {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Pages />
+    </div>
+  )
 }
 
 export default App
