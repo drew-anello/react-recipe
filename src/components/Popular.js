2c06949f29fa6bf9ff9bcf81084a35cb43d13949
Drew Anello <drew.anello@gmail.com>
2022-12-12 11:54:50 -0500

    displaying recipe names and images on page

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 function Popular () {
   // popular action as var and setPopular is acting as function to modify
@@ -19,13 +20,29 @@ function Popular () {
     <div>
       {popular.map(recipe => {
         return (
-          <div>
-            <p>{recipe.title}</p>
-          </div>
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            {popular.map(recipe => {
+              return (
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              )
+            })}
+          </Wrapper>
         )
       })}
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+`
 
 export default Popular
