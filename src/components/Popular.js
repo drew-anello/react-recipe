eee2e40bb88486a100cf5ebd753c12f3ccb8614e
Drew Anello <drew.anello@gmail.com>
2022-12-12 13:09:59 -0500

    adds localStorage to limit API calls

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -11,11 +11,17 @@ function Popular () {
     getPopular()
   }, [])
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    )
-    const data = await api.json()
-    setPopular(data.recipes)
+    const check = localStorage.getItem('popular')
+    if (check) {
+      setPopular(JSON.parse(check))
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      )
+      const data = await api.json()
+      localStorage.setItem('popular', JSON.stringify(data.recipes))
+      setPopular(data.recipes)
+    }
   }
   return (
     // this will have data populate on page.
@@ -33,7 +39,7 @@ function Popular () {
         >
           {popular.map(recipe => {
             return (
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
